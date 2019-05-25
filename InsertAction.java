f7887b923f23281763d66496731a0570b167056b
fed <magister2k4@gmx.de>
2019-05-25 07:08:13 +0200

    add some logic

---
+++ InsertAction.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InsertAction implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String value = arg0.getActionCommand();
+		
+	}
+
+}
